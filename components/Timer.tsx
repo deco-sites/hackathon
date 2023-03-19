a816fc62ca6ed1ef718f24eaaf4139df48dc4997
gimenes <tlgimenes@gmail.com>
2023-03-19 16:45:19 -0300

    add controls

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -11,11 +11,19 @@ const minutes = computed(() =>
   Math.floor(total.value / 60).toFixed(0).padStart(2, "0")
 );
 
+const focus = signal<"minutes" | "seconds" | undefined>(undefined);
+
 let id: number | undefined = undefined;
 
 const start = () => {
   id = setInterval(() => {
-    total.value = Math.max(total.peek() - 1, 0);
+    const newValue = Math.max(total.peek() - 1, 0);
+
+    total.value = newValue;
+
+    if (newValue <= 0) {
+      stop();
+    }
   }, 1e3);
 };
 
@@ -28,7 +36,6 @@ const getStatus = () => id ? "active" : "idle";
 
 if (typeof document !== "undefined") {
   document.addEventListener("keydown", (event) => {
-    console.log(event);
     const status = getStatus();
 
     if (event.key === " " || event.key === "Enter") {
@@ -40,6 +47,24 @@ if (typeof document !== "undefined") {
     } else if (event.key === "r" || event.key === "R") {
       stop();
       total.value = reset.peek();
+    } else if (event.key === "ArrowLeft") {
+      focus.value = focus.value === "minutes" ? undefined : "minutes";
+    } else if (event.key === "ArrowRight") {
+      focus.value = focus.value === "seconds" ? undefined : "seconds";
+    } else if (event.key === "ArrowUp") {
+      if (focus.value === "minutes") {
+        total.value += 60;
+      }
+      if (focus.value === "seconds") {
+        total.value += 1;
+      }
+    } else if (event.key === "ArrowDown") {
+      if (focus.value === "minutes") {
+        total.value = Math.max(0, total.value - 60);
+      }
+      if (focus.value === "seconds") {
+        total.value = Math.max(0, total.value - 1);
+      }
     }
   });
 }
@@ -54,7 +79,11 @@ function Timer() {
         class="flex items-center justify-center text-white h-screen w-screen"
         style={{ fontSize: "25.5vw" }}
       >
-        {minutes}:{seconds}
+        <span class={focus.value === "minutes" ? "text-gray-400" : ""}>
+          {minutes}
+        </span>:<span class={focus.value === "seconds" ? "text-gray-400" : ""}>
+          {seconds}
+        </span>
       </div>
     </>
   );
